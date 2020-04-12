101dea196eeea78852334b4651736177f7c79dfb
Abel Arismendy <39172024+arismendyc@users.noreply.github.com>
2020-04-12 16:59:00 -0500

    Update Header.js

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
     const view = `
         <div class="Header-main">
             <div class="Header-logo">
-                <a href="https://arismendyc.github.io/platzi-spa">
+                <a href="/">
                     100tifi.co
                 </a>
             </div>
